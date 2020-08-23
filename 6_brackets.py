3c7b8bc3bbeb80b92d3bca14e5c7feafabf4c4d1
Nikhil483 <53818269+Nikhil483@users.noreply.github.com>
2020-08-23 16:14:55 +0530

    Update 6_brackets.py

--- 6_brackets.py
+++ 6_brackets.py
@@ -1,20 +1,42 @@
-stack=[]
-import sys
-o=['(','[','{']
-c=[')',']','}']
-s=input()
-for i in s:
-	#print(stack)
-	if i in o :
-		stack.append(i)
-	elif i in c :
-		ch=stack[len(stack)-1]
-		ch1=c[o.index(ch)]
-		if ch1==i : 
-			stack.pop()
-		else : 
-			print("invalid")
-			exit()
-			
+#!/bin/python3
 
-if len(stack)==0 : print("valid")
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the isBalanced function below.
+def isBalanced(s):
+    o=['(','[','{']
+    c=[')',']','}'] 
+    stack = []
+    for i in s:
+        if i in o :
+            stack.append(i)
+        elif i in c :
+            if len(stack) == 0 :
+                return "NO"
+            ch=stack[len(stack)-1]
+            ch1=c[o.index(ch)]
+            if ch1==i : 
+                stack.pop()
+            else : 
+                return "NO"
+
+    if len(stack) == 0 : return "YES"
+    return "NO"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        s = input()
+
+        result = isBalanced(s)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
